8712a21730e0ce998cf89a2e5cca4c0daf9acee3
Simon Martinenq <simon.martinenq@efrei.net>
2022-01-19 01:09:32 +0100

    fin chap 1
    finir de faire fonctionner les fonctions et mettre le cours

--- utils/chap1.js
+++ utils/chap1.js
@@ -11,6 +11,63 @@ function isPrime(n) {
     return true
 }
 
+function pgcd(a, b) {
+    let rk = [a, b]
+
+    let stop = false
+    let i = 2
+    while (!stop) {
+        rk.push(rk[i - 2] % rk[i - 1])
+        if (rk[i] == 0) stop = true
+        i += 1
+    }
+    return rk[i - 2]
+}
+
+function primeFacor(n) {
+    let decompose = []
+    let d = 2
+    let i = 0
+    while (d <= n) {
+        if (n % d == 0) {
+            decompose[i] = d
+            n = Math.floor(n / d)
+            i += 1
+        } else d = d + 1
+    }
+    return decompose
+}
+
+function calculateInverse(a, modulo) {
+    let uk = []
+    let vk = []
+    let rk = []
+    let qk = [0, 0]
+
+    if (a == 1) {
+        return 1
+    } else if (pgcd(a, modulo) == 1) {
+        uk[0] = 1
+        uk[1] = 0
+        vk[0] = 0
+        vk[1] = 1
+        rk[0] = modulo
+        rk[1] = a
+
+        stop = false
+        let i = 2
+        while (!stop) {
+            rk[i] = rk[i - 2] % rk[i - 1]
+            qk[i] = Math.floor(rk[i - 2] / rk[i - 1])
+            uk[i] = uk[i - 2] - (qk[i] * uk[i - 1])
+            vk[i] = vk[i - 2] - (qk[i] * vk[i - 1])
+            if (rk[i] == 0) stop = true
+            i += 1
+        }
+        return vk[i - 2]
+    } else return -666
+}
+
 isPrimeButton.onclick = function() {
     const n = nbrPremier.value
     if (isPrime(n)) {
@@ -21,20 +78,110 @@ isPrimeButton.onclick = function() {
 
 }
 pgcdButton.onclick = function() {
-    alert("coucou")
+    const varPgcd = pgcd(aPgcd.value, bPgcd.value)
+    if (varPgcd == 1) {
+        alert("Le pgcd de " + aPgcd.value + " et " + bPgcd.value + " est: " + varPgcd + ", il sont premier entre eux")
+    } else {
+        alert("Le pgcd de " + aPgcd.value + " et " + bPgcd.value + " est: " + varPgcd)
+    }
 }
 factorisationButton.onclick = function() {
-    alert("coucou")
+    //en déduire le nombre de diviseurs
+    alert("Les facteurs premiers sont: " + primeFacor(primeFactorisation.value))
 }
-euclideEtenduButton.onclick = function() {
-    alert("coucou")
-}
+
+/* euclideEtenduButton.onclick = function() {
+    let uk = []
+    let vk = []
+    let rk = []
+    let qk = [0, 0]
+    let aVar = aEuclide.value
+    let bVar = bEuclide.value
+    let tableEuclideEtendueVar = tableEuclideEtendue
+
+    //vérifier que a > b
+    if (aVar < bVar) {
+        const tmp = aVar
+        aVar = bVar
+        bVar = tmp
+    }
+
+    if (aVar != "" && bVar != "") {
+        uk[0] = (1)
+        uk[1] = (0)
+        vk[0] = (0)
+        vk[1] = (1)
+        rk[0] = (aVar)
+        rk[1] = (bVar)
+
+        stop = false
+        let i = 2
+        while (!stop) {
+            let newLigne = document.createElement("tr")
+            let row1 = document.createElement("td")
+            row1.textContent = rk[i - 2] % rk[i - 1]
+            newLigne.appendChild(row1)
+            rk[i] = rk[i - 2] % rk[i - 1]
+
+            let row2 = document.createElement("td")
+            row2.textContent = Math.floor(rk[i - 2] / rk[i - 1])
+            newLigne.appendChild(row2)
+            qk[i] = Math.floor(rk[i - 2] / rk[i - 1])
+
+            let row3 = document.createElement("td")
+            row3.textContent = uk[i - 2] - qk[i] * uk[i - 1]
+            newLigne.appendChild(row3)
+            uk[i] = uk[i - 2] - qk[i] * uk[i - 1]
+
+            let row4 = document.createElement("td")
+            row4.textContent = vk[i - 2] - qk[i] * vk[i - 1]
+            newLigne.appendChild(row4)
+            vk[i] = vk[i - 2] - qk[i] * vk[i - 1]
+
+            tableEuclideEtendueVar.appendChild(newLigne)
+            if (rk[i] == 0) stop = true
+            i += 1
+        }
+        //return mod(vk[i - 2], modulo)
+    } else alert("il manque des variables")
+        //else return -1
+
+} */
+
 powerButton.onclick = function() {
     alert("coucou")
 }
+
+buttonNInverser.onclick = function() {
+    let nInvererVar = nInverer.value
+    let modInverserVar = modInverser.value
+    if (nInvererVar == "" || modInverserVar == "") {
+        alert("Il manque des informations")
+    } else {
+        alert(calculateInverse(nInvererVar, modInverserVar))
+    }
+}
+
 invertibleListButton.onclick = function() {
-    alert("coucou")
+    //il suffit de trouver la premiere moitiee des inverse 
+    //le reste est simplement le symétrique 2 -> -2
+    let listInvertible = []
+    for (let index = 0; index < nInvList.value; index++) {
+        const invVar = calculateInverse(index, nInvList.value)
+        if (invVar != -666 && invVar != 0) {
+            listInvertible.push(invVar)
+        }
+    }
+    alert(listInvertible)
 }
 eqButton.onclick = function() {
-    alert("coucou")
+    if (pgcd(aEq.value, nEq.value) == 1) {
+        alert("x = " + calculateInverse(aEq.value, nEq.value) * bEq.value)
+    } else if (bEq.value % pgcd(aEq.value, nEq.value) == 0) {
+        let solTab = []
+        for (let index = 0; index < aEq.value - 1; index++) {
+            solTab.push(bEq.value / aEq.value + nEq.value * index / aEq.value)
+        }
+        alert(solTab)
+    } else alert("no solution")
 }
